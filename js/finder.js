1e7aa19e1cb15aed94ee991661fdf256e1b08e02
Xia <tiancheng.xia2001@gmail.com>
2021-02-14 10:36:47 +0100

    Finding adjacent nodes improvement

--- js/finder.js
+++ js/finder.js
@@ -16,24 +16,6 @@ function animation_speed(speed) {
     ANIMATION_SPEED = speed;
 }
 
-function adjacent_nodes_d(grid, coord) {
-    var near = [];
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            if (i == 0 && j == 0) {
-                continue;
-            }
-
-            if ((coord.getX() + i >= 0 && coord.getY() + j >= 0) && (coord.getX() + i < grid.getColumns() && coord.getY() + j < grid.getRows())) {
-                if (grid.getMatrix()[coord.getX() + i][coord.getY() + j] != WALL_TILE) {
-                    near.push(new Coord(coord.getX() + i, coord.getY() + j));
-                }
-            }
-        }
-    }
-    return near;
-}
-
 function adjacent_nodes_s(grid, coord) {
     var near = [];
     if (coord.getX() + 1 < grid.getColumns()) {
@@ -54,6 +36,31 @@ function adjacent_nodes_s(grid, coord) {
     if (coord.getY() - 1 >= 0) {
         if (grid.getMatrix()[coord.getX()][coord.getY() - 1] != WALL_TILE) {
             near.push(new Coord(coord.getX(), coord.getY() - 1));
+        }
+    }
+    return near;
+}
+
+function adjacent_nodes_d(grid, coord) {
+    var near = adjacent_nodes_s(grid, coord);
+    if (coord.getX() + 1 < grid.getColumns() && coord.getY() + 1 < grid.getRows()) {
+        if (grid.getMatrix()[coord.getX()+1][coord.getY()+1] != WALL_TILE) {
+            near.push(new Coord(coord.getX()+1, coord.getY()+1));
+        }
+    }
+    if (coord.getX() - 1 >= 0 && coord.getY() - 1 >= 0) {
+        if (grid.getMatrix()[coord.getX()-1][coord.getY()-1] != WALL_TILE) {
+            near.push(new Coord(coord.getX()-1, coord.getY()-1));
+        }
+    }
+    if (coord.getX() - 1 >= 0 && coord.getY() + 1 < grid.getRows()) {
+        if (grid.getMatrix()[coord.getX()-1][coord.getY()+1] != WALL_TILE) {
+            near.push(new Coord(coord.getX()-1, coord.getY()+1));
+        }
+    }
+    if (coord.getX() + 1 < grid.getColumns() && coord.getY() - 1 >= 0) {
+        if (grid.getMatrix()[coord.getX()+1][coord.getY()-1] != WALL_TILE) {
+            near.push(new Coord(coord.getX()+1, coord.getY()-1));
         }
     }
     return near;
